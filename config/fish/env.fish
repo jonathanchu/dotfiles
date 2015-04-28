bf262e32c9b591442d1a3ddd157cffa35eb7e0c8
Jonathan Chu <jonathan.chu@me.com>
2015-04-28 16:13:00 -0400

    Update path for postgres 9.4

--- config/fish/env.fish
+++ config/fish/env.fish
@@ -2,5 +2,5 @@ append-to-path ~/projects/git-toolbelt
 
 set -gx WORKON_HOME ~/.virtualenvs
 
-append-to-path /Applications/Postgres.app/Contents/Versions/9.3/bin
+append-to-path /Applications/Postgres.app/Contents/Versions/9.4/bin
 append-to-path ~/.local/bin
