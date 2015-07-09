6af4e19a8d34cf00290faaa0a90ab81713929c7b
Jonathan Chu <jonathan.chu@me.com>
2015-07-09 00:28:28 -0400

    Add refactor-nrepl

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,2 +1,2 @@
-{:user {:plugins [[cider/cider-nrepl "0.9.0"]]
-        :dependencies [[org.clojure/tools.nrepl "0.2.10"]]}}
+{:user {:plugins [[cider/cider-nrepl "0.9.0"] [refactor-nrepl "1.1.0"]]
+        :dependencies [[org.clojure/tools.nrepl "0.2.7"]]}}
