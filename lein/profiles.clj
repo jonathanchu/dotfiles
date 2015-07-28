0cb7a7bd1a8ab64d8afd89386e97f215b2038c27
Jonathan Chu <jonathan.chu@me.com>
2015-07-28 00:52:28 -0400

    Sync up clj-refactor and refactor-repl versions

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,3 +1,3 @@
 {:user {:plugins [[cider/cider-nrepl "0.10.0-snapshot"]
-                  [refactor-nrepl "1.1.0"]]
+                  [refactor-nrepl "1.2.0-SNAPSHOT"]]
         :dependencies [[org.clojure/tools.nrepl "0.2.7"]]}}
