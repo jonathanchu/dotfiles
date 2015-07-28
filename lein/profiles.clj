add2fc9e8ffe7ad0845a699cf2bb146d1729c271
Jonathan Chu <jonathan.chu@me.com>
2015-07-28 00:50:58 -0400

    Drop plugins on new lines
    This way we can track changes to each plugin easily instead of seeing a
    diff for a whole line change.

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,2 +1,3 @@
-{:user {:plugins [[cider/cider-nrepl "0.10.0-snapshot"] [refactor-nrepl "1.1.0"]]
+{:user {:plugins [[cider/cider-nrepl "0.10.0-snapshot"]
+                  [refactor-nrepl "1.1.0"]]
         :dependencies [[org.clojure/tools.nrepl "0.2.7"]]}}
