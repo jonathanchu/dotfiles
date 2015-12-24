2384aceab7d12491528e207114168a4097dcd0e3
Jonathan Chu <jonathan.chu@me.com>
2015-12-24 01:29:05 -0500

    Add lein-pprint to config

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,3 +1,4 @@
 {:user {:plugins [[cider/cider-nrepl "0.11.0-SNAPSHOT"]
-                  [refactor-nrepl "1.2.0-SNAPSHOT"]]
+                  [refactor-nrepl "1.2.0-SNAPSHOT"]
+                  [lein-pprint "1.1.1"]]
         :dependencies [[org.clojure/tools.nrepl "0.2.12"]]}}
