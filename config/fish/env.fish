45c9f99a44da88df10ce4a514dbf0df276398949
Jonathan Chu <jonathan.chu@me.com>
2017-12-12 11:04:20 -0500

    No cows for ansible

--- config/fish/env.fish
+++ config/fish/env.fish
@@ -1,7 +1,7 @@
-append-to-path ~/projects/git-toolbelt
-
 set -gx WORKON_HOME ~/.virtualenvs
 set -gx PYTHONPATH ~/.local/bin
+set -gx ANSIBLE_NOCOWS 1
 
+append-to-path ~/projects/git-toolbelt
 append-to-path ~/.local/bin
 append-to-path /Applications/Postgres.app/Contents/Versions/9.6/bin
