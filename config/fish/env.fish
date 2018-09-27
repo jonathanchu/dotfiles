5f43e906a2991d0a4fa63e1a617a7b7b6680e7ec
Jonathan Chu <me@jonathanchu.is>
2018-09-27 01:16:23 -0500

    Remove unused paths

--- config/fish/env.fish
+++ config/fish/env.fish
@@ -2,6 +2,4 @@ set -gx WORKON_HOME ~/.virtualenvs
 set -gx PYTHONPATH ~/.local/bin
 set -gx ANSIBLE_NOCOWS 1
 
-append-to-path ~/projects/git-toolbelt
 append-to-path ~/.local/bin
-append-to-path /Applications/Postgres.app/Contents/Versions/9.6/bin
