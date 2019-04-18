db3ef5246bc98b0b76279a8fd99a234a0aa956be
Jonathan Chu <me@jonathanchu.is>
2019-04-18 00:27:06 -0400

    Add path from pipsi

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -116,3 +116,7 @@ if not functions -q fisher
     curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
     fish -c fisher
 end
+
+# added by pipsi (https://github.com/mitsuhiko/pipsi)
+set -x PATH /Users/jonathan/.local/bin $PATH
+
