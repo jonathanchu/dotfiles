e4aec4951f4bd34ebedc742e5cb798128b568d6c
Jonathan Chu <me@jonathanchu.is>
2019-12-10 12:56:18 -0500

    Update lein plugin versions
    ¯\_(ツ)_/¯

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,4 +1,4 @@
-{:user {:plugins [[cider/cider-nrepl "0.14.0-SNAPSHOT"]
-                  [refactor-nrepl "2.3.0-SNAPSHOT"]
-                  [lein-pprint "1.1.2"]]
-        :dependencies [[org.clojure/tools.nrepl "0.2.12"]]}}
+{:user {:plugins [[cider/cider-nrepl "0.23.0-SNAPSHOT"]
+                  [refactor-nrepl "2.5.0-SNAPSHOT"]
+                  [lein-pprint "1.2.0"]]
+        :dependencies [[org.clojure/tools.nrepl "0.7.0-SNAPSHOT"]]}}
