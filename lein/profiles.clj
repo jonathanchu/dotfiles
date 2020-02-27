0cd7c22dfcc3919dfda274e2401bdd60c516350a
Jonathan Chu <me@jonathanchu.is>
2020-02-27 12:21:34 -0500

    Serverless stuff

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,4 +1,4 @@
-{:user {:plugins [[cider/cider-nrepl "0.23.0-SNAPSHOT"]
-                  [refactor-nrepl "2.5.0-SNAPSHOT"]
-                  [lein-pprint "1.2.0"]]
-        :dependencies [[org.clojure/tools.nrepl "0.7.0-SNAPSHOT"]]}}
+;; {:user {:plugins [[cider/cider-nrepl "0.23.0-SNAPSHOT"]
+;;                   [refactor-nrepl "2.5.0-SNAPSHOT"]
+;;                   [lein-pprint "1.2.0"]]
+;;         :dependencies [[org.clojure/tools.nrepl "0.7.0-SNAPSHOT"]]}}
