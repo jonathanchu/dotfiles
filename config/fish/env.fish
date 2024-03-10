11edadf210c48756c2136a9bc62cb69d99dab530
Jonathan Chu <me@jonathanchu.is>
2024-03-09 19:55:38 -0500

    Add nvm function for fish env

--- config/fish/env.fish
+++ config/fish/env.fish
@@ -3,3 +3,15 @@ set -gx PYTHONPATH ~/.local/bin
 set -gx ANSIBLE_NOCOWS 1
 
 append-to-path ~/.local/bin
+
+# Nvm {{{
+
+function nvm
+    # NOTE: nvm does not support Fish shell, so we we'll have to wrap it in the
+    # "bass" utility, which allows execution of classic Bash shell scripts,
+    # which monitors for any changed environment variables, and applies these
+    # to the Fish shell env.
+    bass source ~/.nvm/nvm.sh --no-use ';' nvm $argv
+end
+
+# }}}
