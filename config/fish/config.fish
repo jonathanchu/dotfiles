1e603a35c8636dacda38493ce97ba9647d8e35fe
Jonathan Chu <me@jonathanchu.is>
2024-03-09 19:54:55 -0500

    Update fish config for nvm dir and latest PATH

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -97,7 +97,11 @@ end
 
 set PYENV_ROOT $HOME/.pyenv
 set POSTGRES_ROOT /Applications/Postgres.app/Contents/Versions/latest/bin
-set PATH /usr/local/bin /usr/local/sbin $HOME/.local/bin $PYENV_ROOT/shims $HOME/bin $POSTGRES_ROOT $PATH
+set PATH /usr/local/bin /usr/local/sbin $HOME/.local/bin $PYENV_ROOT/shims $HOME/bin /opt/homebrew/bin $POSTGRES_ROOT $PATH
+
+# export NVM_DIR="$HOME/.nvm"
+# [ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh" # This loads nvm
+# [ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" # This loads nvm bash_completion
 
 if status --is-interactive
     source (pyenv init - | psub)
