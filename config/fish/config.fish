e8b79c438bd3cc1b48abff1838cfd5cf71d86f35
Jonathan Chu <me@jonathanchu.is>
2025-11-18 05:37:36 -0500

    Remove pyenv and replace with uv

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -95,17 +95,12 @@ if status --is-interactive
     . ~/.config/fish/private.fish
 end
 
-set PYENV_ROOT $HOME/.pyenv
 set POSTGRES_ROOT /Applications/Postgres.app/Contents/Versions/latest/bin
-set PATH /usr/local/bin /usr/local/sbin $HOME/.local/bin $PYENV_ROOT/shims $HOME/bin /opt/homebrew/bin $POSTGRES_ROOT $PATH
+set PATH /usr/local/bin /usr/local/sbin $HOME/.local/bin $HOME/.cargo/bin $HOME/bin /opt/homebrew/bin $POSTGRES_ROOT $PATH
 
 # export NVM_DIR="$HOME/.nvm"
 # [ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh" # This loads nvm
 # [ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" # This loads nvm bash_completion
-
-if status --is-interactive
-    source (pyenv init - | psub)
-end
 
 set -gx PYTHONDONTWRITEBYTECODE 1
 
