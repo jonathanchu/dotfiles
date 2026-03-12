0772a6ef4e201b5f81bc8cb2ba368ec5fb38eede
Jonathan Chu <me@jonathanchu.is>
2026-03-11 23:25:22 -0400

    Update npm-globals.sh to match current eglot LSP setup

--- npm-globals.sh
+++ npm-globals.sh
@@ -1,5 +1,2 @@
-npm install -g @emacs-lsp/emacs-language-server
-
-npm install -g eslint
-npm install -g bash-language-server
+npm install -g pyright
 npm install -g typescript-language-server typescript
