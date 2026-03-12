23b00be5d45315178705b6d357c4797c14b69857
Jonathan Chu <me@jonathanchu.is>
2026-03-11 23:38:35 -0400

    Remove fisher plugin manager config

--- config/fisher/github.com/edc/bass/test/fixtures/alias.sh
+++
@@ -1,1 +0,0 @@
-alias k?="echo hello"
--- config/fisher/github.com/edc/bass/test/test_trailing_semicolon.fish
+++
@@ -1,7 +0,0 @@
-set root (dirname (dirname (status -f)))
-source $root/functions/bass.fish
-
-bass (sh $root/test/fixtures/trailing_semicolon.sh)
-and if [ "$SEMICOLON_RSTRIPPED" = "1" ]
-	echo (set_color green)Success(set_color normal)
-end
